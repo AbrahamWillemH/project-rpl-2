360074be534d666552416e262e5cdb40549e3ed8
Abe <abraham120604@gmail.com>
2024-06-18 00:16:32 +0700

    added proxy

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import { useEffect} from "react"
+import { useEffect } from "react"
 
 // pages & components
 import Home from './pages/Home'
