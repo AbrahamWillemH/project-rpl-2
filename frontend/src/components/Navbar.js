5827cc583daa2b13ed117a18e8eac698f94e25c2
Abe <abraham120604@gmail.com>
2024-06-18 19:13:02 +0700

    update

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -3,9 +3,12 @@ import '../index.css';
 import '../colors.css'
 import nav_logo from '../assets/logo-nav.png'
 import { useLogout } from "../hooks/useLogout";
+import { useAuthContext } from "../hooks/useAuthContext";
 
 const Navbar = () => {
   const {logout} = useLogout()
+  const {user} = useAuthContext()
+
   const handleClick = () => {
     logout()
   }
@@ -17,15 +20,23 @@ const Navbar = () => {
           <img src={nav_logo} alt="logo" width="45" />
           <p className="ml-5 font-bold text-white">HEALTH TRACKER</p>
         </Link>
-        <div className="w-[600px] flex justify-between">
-          <Link to="/dashboard" className="py-[7px]">Dashboard</Link>
-          <Link to="/workouts" className="py-[7px]">Workouts</Link>
-          <Link to="/meals" className="py-[7px]">Meals</Link>
-          <Link to="" className="py-[7px]">Information</Link>
-          <Link to="login.html" onClick={handleClick} className="btn-primary px-[24px] py-[7px] rounded-md">
-            <p className="text-white">Log Out</p>
-          </Link>
-        </div>
+        {user && (
+          <div className="w-[600px] flex justify-between">
+            <Link to="/dashboard" className="py-[7px]">Dashboard</Link>
+            <Link to="/workouts" className="py-[7px]">Workouts</Link>
+            <Link to="/meals" className="py-[7px]">Meals</Link>
+            <Link to="/articles" className="py-[7px]">Information</Link>
+            <Link to="/" onClick={handleClick} className="btn-primary px-[24px] py-[7px] rounded-md">
+              <p className="text-white">Log Out</p>
+            </Link>
+          </div>
+        )}
+        {!user && (
+          <div className="w-[200px] flex gap-4">
+            <Link to='/login' className="btn-primary px-[24px] py-[7px] rounded-md">Login</Link>
+            <Link to='/signup' className="btn-primary px-[24px] py-[7px] rounded-md">Signup</Link>
+          </div>
+        )}
       </div>
     </nav>
   )
